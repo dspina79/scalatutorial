ab95db6311d0ddaaf70ca9f850736e42d7525629
Dave Spina <dspina79@gmail.com>
2021-08-12 21:21:35 -0400

    Basic scala loop

---
+++ ScalaTutorial1.scala
@@ -0,0 +1,10 @@
+object ScalaTutorial1 {
+    def main(args: Array[String]) {
+        var i = 0;
+
+        while(i < 10) {
+            println("There is a number: " + i);
+            i += 1;
+        }
+    }
+}
